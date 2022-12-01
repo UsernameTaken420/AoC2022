c2014e8840823dbb876ebb6cdba8277fa6b44a71
UsernameTaken420 <Nico_Gabin@yahoo.com>
2022-12-01 16:30:05 -0300

    day1 part one completed

--- day1/day1.py
+++ day1/day1.py
@@ -1,2 +1,12 @@
 calorieList = open("input.txt", "r")
-print(calorieList)
+elves = []
+elvesCount = 0
+elves.append(0)
+for item in calorieList:
+    if item in ['\n', '\r\n']:
+        elves.append(0)
+        elvesCount += 1
+    else:
+        elves[elvesCount] += int(item)
+elves.sort()
+print(elves)
