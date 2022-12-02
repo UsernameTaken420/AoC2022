64eda92f136d20de3a6d1b72c8baad225401fd66
UsernameTaken420 <Nico_Gabin@yahoo.com>
2022-12-02 10:36:31 -0300

    functionized round result

--- day2/day2.py
+++ day2/day2.py
@@ -1,5 +1,26 @@
-file = open("input.txt", "r")
-for round in file:
-    match round[0]:
+def roundResult(opPlay, mePlay):
+    match opPlay:
         case "A":
-            print("A")
+            opPlay = 1
+        case "B":
+            opPlay = 2
+        case "C":
+            opPlay = 3
+    match mePlay:
+        case "X":
+            points = 1
+        case "Y":
+            points = 2
+        case "Z":
+            points = 3
+    if (points > opPlay):
+        points += 6
+    elif (points == opPlay):
+        points += 3
+    return points
+
+file = open("testInput.txt", "r")
+totalScore = 0
+for round in file:
+    totalScore += roundResult(round[0], round[2])
+print(totalScore)
